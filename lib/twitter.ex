88dbb4958fd6fd4216d90390ba3b826b71c3d8fe
gabriele.lana <gabriele.lana@cleancode.it>
2015-07-12 23:08:43 +0200

    Read scenario cleared

--- lib/twitter.ex
+++ lib/twitter.ex
@@ -1,13 +1,20 @@
 defmodule Twitter do
   alias Twitter.User
+  alias Twitter.Message
 
   def run(line) when is_binary(line) do
     {:ok, command} = Command.parse(line)
     run(command)
   end
 
-  def run({:post, at, user, message}), do: User.post(user, message, at)
-  def run({:read, at, user}), do: User.read(user, at)
+  def run({:post, at, user, message}) do
+    User.post(user, message, at)
+  end
+
+  def run({:read, at, user}) do
+    User.read(user, at) |> Enum.map(&Message.format(&1, at)) |> Enum.each(&IO.puts/1)
+  end
+
   def run({:follow, at, user, who}), do: User.follow(user, who, at)
   def run({:wall, at, user}), do: User.wall(user, at)
 end
--- lib/user.ex
+++ lib/user.ex
@@ -1,8 +1,10 @@
 defmodule Twitter.User do
   alias Twitter.User
+  alias Twitter.Timeline
+  alias Twitter.Message
   use GenServer
 
-  defstruct name: ""
+  defstruct name: "", timeline: nil, followers: [], following: []
 
   def post(user, message, at \\ now) do
     GenServer.cast(locate(user), {:post, message, at})
@@ -21,18 +23,19 @@ defmodule Twitter.User do
   end
 
   def init(name) do
-    {:ok, %User{name: name}}
+    {:ok, %User{name: name, timeline: Timeline.new}}
   end
 
-  def handle_cast({:post, _message, _at}, user) do
-    {:noreply, user}
+  def handle_cast({:post, message, at}, user) do
+    timeline = Timeline.push(user.timeline, %Message{at: at, from: user.name, text: message})
+    {:noreply, %User{user|timeline: timeline}}
   end
   def handle_cast({:follow, _who, _at}, user) do
     {:noreply, user}
   end
 
-  def handle_call({:read, _at}, _, user) do
-    {:reply, :read, user}
+  def handle_call({:read, at}, _, user) do
+    {:reply, Timeline.from(user.timeline, user.name, at), user}
   end
   def handle_call({:wall, _at}, _, user) do
     {:reply, :wall, user}
@@ -43,15 +46,15 @@ defmodule Twitter.User do
     register_name = String.to_atom(name)
     case Process.whereis(register_name) do
       nil ->
-        {:ok, pid} = start_link(register_name)
+        {:ok, pid} = start_link(name, register_name)
         pid
       pid ->
         pid
     end
   end
 
-  defp start_link(name) do
-    GenServer.start_link(User, name, name: name)
+  defp start_link(name, register_name) do
+    GenServer.start_link(User, name, name: register_name)
   end
 
   defp now, do: :calendar.local_time
